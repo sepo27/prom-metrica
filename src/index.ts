0b9dab9ddd64d96e5ea1e024d501da37caa1ad31
Sepo <sepo@one.com>
2021-09-12 09:46:55 +0400

    Add index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-// TODO
+import { PromClient } from './client/PromClient';
+import { metricsHandler } from './handler/metricsHandler';
+import { HttpReqDurationMetric } from './metric/http/HttpReqDurationMetric';
+import { httpRequestDurationMiddleware } from './middleware/httpRequestDurationMiddleware';
+
+export {
+  PromClient,
+  HttpReqDurationMetric,
+  httpRequestDurationMiddleware,
+  metricsHandler,
+};
