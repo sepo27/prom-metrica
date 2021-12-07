24a1e0e33c16e80195954f2805ef3b8e6f9cab1a
Sepo <sepo@one.com>
2021-12-07 22:14:09 +0400

    Export MetricaKit type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { makeMetricaKit } from './kit/makeMetricaKit';
+import { MetricaKit } from './kit/types';
 import { HttpHistogramMetric } from './metric/http/histogram/HttpHistogramMetric';
 
-export { HttpHistogramMetric, makeMetricaKit };
+export { HttpHistogramMetric, makeMetricaKit, MetricaKit };
