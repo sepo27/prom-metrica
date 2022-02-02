42b8c4b9cf62c2d1b021c75635d98d7e28175ca4
Sepo <sepo@one.com>
2022-02-02 10:09:09 +0400

    no-jira: Include promClient into export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
+import * as promClient from 'prom-client';
 import { makeMetricaKit } from './kit/makeMetricaKit';
 import { MetricaKit } from './kit/types';
 import { HttpHistogramMetric } from './metric/http/histogram/HttpHistogramMetric';
 
-export { HttpHistogramMetric, makeMetricaKit, MetricaKit };
+export {
+  HttpHistogramMetric,
+  makeMetricaKit,
+  MetricaKit,
+  promClient,
+};
